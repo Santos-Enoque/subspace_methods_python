fcfd970b969531a4e06be0e9abcaabc9e6906d8f
santos-enoque <santosenoque.ss@gmail.com>
2023-08-02 13:55:31 +0900

    added math utils and evaluation methods

--- src/utils/base.py
+++ src/utils/base.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import eigh
 
 def mean_square_elements(matrix):
     """
@@ -72,9 +73,203 @@ def canonical_angle_matrix(X, Y):
 
     return C
 
-# Define basis matrices for testing canonical_angle and canonical_angle_matrix
+def _eigh(X, eigvals=None):
+    """
+    A wrapper function of numpy.linalg.eigh and scipy.linalg.eigh.
 
-X = [np.array([[1, 2], [3, 4]]), np.array([[5, 6], [7, 8]])]
-Y = [np.array([[ 2, 3], [ 5, 6]]), np.array([[8, 9], [11, 12]])]
-print(canonical_angle(X[0], X[1]))
-print(canonical_angle_matrix(X, Y))
+    Parameters
+    ----------
+    X: array-like, shape (a, a)
+        A symmetric matrix for which to compute eigenvalues and eigenvectors.
+
+    eigvals: tuple, (lo, hi)
+        Indices of the smallest and largest (in ascending order) eigenvalues and corresponding eigenvectors
+        to be returned: 0 <= lo <= hi <= M-1. If omitted, all eigenvalues and eigenvectors are returned.
+
+    Returns
+    -------
+    e: array-like, shape (a) or (n_dims)
+        Eigenvalues in descending order.
+
+    V: array-like, shape (a, a) or (a, n_dims)
+        Corresponding eigenvectors, where each column is an eigenvector.
+    """
+    if eigvals != None:
+        e, V = eigh(X, eigvals=eigvals)
+    else:
+        # numpy's eigh is faster than scipy's when all calculating eigenvalues and eigenvectors
+        e, V = np.linalg.eigh(X)
+
+    e, V = e[::-1], V[:, ::-1]
+
+    return e, V
+    
+    
+def _eigen_basis(X, eigvals=None):
+    """
+    Compute the eigenbasis of the input matrix.
+
+    Parameters
+    ----------
+    X: array-like, shape (a, a)
+        Input matrix for which to compute the eigenbasis.
+
+    eigvals: tuple (low, high), default=None
+        Indices of the smallest and largest (in ascending order) eigenvalues and corresponding eigenvectors
+        to be returned: 0 <= lo <= hi <= M-1. If omitted, all eigenvalues and eigenvectors are returned.
+
+    Returns
+    -------
+    e: array-like, shape (a) or (n_dims)
+        Eigenvalues in descending order.
+
+    V: array-like, shape (a, a) or (a, n_dims)
+        Corresponding eigenvectors, where each column is an eigenvector.
+    """
+
+    try:
+        e, V = _eigh(X, eigvals=eigvals)
+    except np.linalg.LinAlgError:
+        # if it not converges, try with tiny salt
+        salt = 1e-8 * np.eye(X.shape[0])
+        e, V = eigh(X + salt, eigvals=eigvals)
+
+    return e, V
+
+
+def _get_eigvals(n, n_subdims, higher):
+    """
+        Calculate the range of eigenvalues to compute in terms of indices.
+
+    Parameters
+    ----------
+    n: int
+        Total number of dimensions in the input data.
+
+    n_subdims: int, default=None
+        Desired number of dimensions in the subspace. If None, all dimensions are used.
+
+    higher: boolean, default=True
+        If True, compute the largest `n_subdim` eigenvalues. If False, compute the smallest.
+
+    Returns
+    -------
+    eigvals: tuple of 2 integers (low, high)
+        Indices of the smallest and largest (in ascending order) eigenvalues to be computed.
+    """
+
+    if n_subdims is None:
+        return None
+
+    if higher:
+        low = max(0, n - n_subdims)
+        high = n - 1
+    else:
+        low = 0
+        high = min(n - 1, n_subdims - 1)
+
+    return low, high 
+
+
+
+def subspace_bases(X, n_subdims=None, higher=True, return_eigvals=False):
+    """
+    Return subspace basis using PCA
+
+    Parameters
+    ----------
+    X: array-like, shape (n_dimensions, n_vectors)
+        data matrix
+    n_subdims: integer
+        number of subspace dimension
+    higher: bool
+        if True, this function returns eigenvectors collesponding
+        top-`n_subdims` eigenvalues. default is True.
+    return_eigvals: bool
+        if True, this function also returns eigenvalues.
+    Returns
+    -------
+    V: array-like, shape (n_dimensions, n_subdims)
+        bases matrix
+    w: array-like shape (n_subdims)
+        eigenvalues
+    """
+
+    if X.shape[0] <= X.shape[1]:
+        eigvals = _get_eigvals(X.shape[0], n_subdims, higher)
+        # get eigenvectors of autocorrelation matrix X @ X.T
+        w, V = _eigen_basis(X @ X.T, eigvals=eigvals)
+    else:
+        # use linear kernel to get eigenvectors
+        A, w = dual_vectors(X.T @ X, n_subdims=n_subdims, higher=higher)
+        V = X @ A
+
+    if return_eigvals:
+        return V, w
+    else:
+        return V
+
+
+def dual_vectors(K, n_subdims=None, higher=True, eps=1e-6):
+    """
+    Calc dual representation of vectors in kernel space
+
+    Parameters:
+    -----------
+    K :  array-like, shape: (n_samples, n_samples)
+        Grammian Matrix of X: K(X, X)
+    n_subdims: int, default=None
+        Number of vectors of dual vectors to return
+    higher: boolean, default=None
+        If True, this function returns eigenbasis corresponding to 
+            higher `n_subdims` eigenvalues in descending order.
+        If False, this function returns eigenbasis corresponding to 
+            lower `n_subdims` eigenvalues in descending order.
+    eps: float, default=1e-20
+        lower limit of eigenvalues
+
+    Returns:
+    --------
+    A : array-like, shape: (n_samples, n_samples)
+        Dual replesentation vectors.
+        it satisfies lambda[i] * A[i] @ A[i] == 1, where lambda[i] is i-th biggest eigenvalue
+    e:  array-like, shape: (n_samples, )
+        Eigen values descending sorted
+    """
+
+    eigvals = _get_eigvals(K.shape[0], n_subdims, higher)
+    e, A = _eigen_basis(K, eigvals=eigvals)
+
+    # replace if there are too small eigenvalues
+    e[(e < eps)] = eps
+
+    A = A / np.sqrt(e)
+
+    return A, e
+
+
+def cross_similarities(refs, inputs):
+    """
+    Calc similarities between each reference spaces and each input subspaces
+
+    Parameters:
+    -----------
+    refs: list of array-like (n_dims, n_subdims_i)
+    inputs: list of array-like (n_dims, n_subdims_j)
+
+    Returns:
+    --------
+    similarities: array-like, shape (n_refs, n_inputs)
+    """
+
+    similarities = []
+    for _input in inputs:
+        sim = []
+        for ref in refs:
+            sim.append(mean_square_elements(ref.T @ _input))
+        similarities.append(sim)
+
+    similarities = np.array(similarities)
+
+    return similarities
+
--- test_base.py
+++ test_base.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('src/utils')
-from base import mean_square_elements, canonical_angle, canonical_angle_matrix
+from base import mean_square_elements, canonical_angle, canonical_angle_matrix, _eigh, _get_eigvals, _eigen_basis, subspace_bases, dual_vectors
 import unittest 
 import numpy as np
 
@@ -10,6 +10,13 @@ class TestBase(unittest.TestCase):
     # Define basis matrices for testing canonical_angle and canonical_angle_matrix
        self.X = [np.array([[1, 2], [3, 4]]), np.array([[5, 6], [7, 8]])]
        self.Y = [np.array([[2, 3], [5, 6]]), np.array([[8, 9], [11, 12]])]
+        # This is a symmetric matrix
+       self.matrix2 = np.array([[4, 1], [1, 4]])
+       self.K = np.array([[4, 4], [3, 3]])
+
+        # These are the expected eigenvalues and eigenvectors of matrix2
+       self.expected_eigenvalues = np.array([5, 3])
+       self.expected_eigenvectors = np.array([[0.70710678, -0.70710678], [0.70710678, 0.70710678]])
 
     def test_mean_square_elements(self):
         self.assertEqual(mean_square_elements(self.matrix1), 15)
@@ -22,6 +29,51 @@ class TestBase(unittest.TestCase):
         data = [[1099., 6031.], [6419., 35639.]]
         expected_output = np.array(data)
         np.testing.assert_allclose(canonical_angle_matrix(self.X, self.Y), expected_output)
+    
+    def test_eigh(self):
+
+        eigenvalues, eigenvectors = _eigh(self.matrix2)
+
+        # Check that the computed eigenvalues match the expected eigenvalues
+        np.testing.assert_array_almost_equal(eigenvalues, self.expected_eigenvalues, decimal=8)
+
+        # Check that the computed eigenvectors match the expected eigenvectors
+        np.testing.assert_array_almost_equal(eigenvectors, self.expected_eigenvectors, decimal=8)
+
+    def test_get_eigvals(self):
+        # Here, we assume n is 10 and n_subdims is 3
+        n = 10
+        n_subdims = 3
+
+        # When higher is True
+        expected_eigvals_higher = (7, 9) # Expected to return 7, 8, 9
+        self.assertEqual(_get_eigvals(n, n_subdims, True), expected_eigvals_higher)
+
+        # When higher is False
+        expected_eigvals_lower = (0, 2) # Expected to return 0, 1, 2
+        self.assertEqual(_get_eigvals(n, n_subdims, False), expected_eigvals_lower)
+    
+    def test_eigen_basis(self):
+        # Without specifying eigvals
+        eigenvalues, eigenvectors = _eigen_basis(self.matrix2)
+
+        # Check that the computed eigenvalues match the expected eigenvalues
+        np.testing.assert_array_almost_equal(eigenvalues, self.expected_eigenvalues, decimal=8)
+
+        # Check that the computed eigenvectors match the expected eigenvectors
+        np.testing.assert_array_almost_equal(eigenvectors, self.expected_eigenvectors, decimal=8)
+
+        # When specifying eigvals
+        eigenvalues, eigenvectors = _eigen_basis(self.matrix2, eigvals=(0, 0))  # here it only return the first eigenvalue/vector
+
+        # # Check that the computed eigenvalue matches the first expected eigenvalue
+        np.testing.assert_almost_equal(eigenvalues[0], self.expected_eigenvalues[1], decimal=8)
+
+
+    def test_subspace_bases(self):
+        expected_bases = np.array([[-0.8, 0.6], [-0.6, -0.8]])
+        np.testing.assert_array_almost_equal(subspace_bases(self.K), expected_bases, decimal=8)
+
 
 if __name__ == '__main__':
     unittest.main()
