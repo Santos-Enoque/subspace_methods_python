8c76af7ec02d01d709f2a2cbde5d083f7df9742a
santos-enoque <santosenoque.ss@gmail.com>
2023-08-29 18:44:48 +0900

    used rffMSM with fashion Mnist

--- iris_msm.py
+++ iris_msm.py
@@ -8,7 +8,7 @@ iris_data = load_iris()
 X, y = iris_data.data, iris_data.target
 train_X, train_y, test_X, test_y = DataTransformer(X, y).transform()
 
-model = KernelMSM(n_subdims=5, sigma=0.1, normalize=True, faster_mode=True)
+model = KernelMSM(n_subdims=5, sigma=0.01, normalize=True, faster_mode=True)
 model.fit(train_X, train_y)
 pred = model.predict(test_X)
 print(f"pred: {pred}\ntrue: {test_y}\naccuracy: {(pred == test_y).mean()}")
